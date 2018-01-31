98309e0517f00269f02ed368973d13895bb6f014
chriswillphoto <christophernekich@gmail.com>
2018-02-01 02:55:10 +1100

    fiddled with responsiveness reworded headings changed aboutme ender

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-
-
 $('.tag').on('click', function() {
-  $(".selected").removeClass('selected')
-  $('.not-selected').removeClass('not-selected')
-  $(this).parent().removeClass('hover').addClass('selected')
-  $(this).parent().siblings(":not(.tagline)").addClass('not-selected hover')
-})
+  $('.selected').removeClass('selected');
+  $('.not-selected').removeClass('not-selected');
+  $(this)
+    .parent()
+    .removeClass('hover')
+    .addClass('selected');
+  $(this)
+    .parent()
+    .siblings(':not(.tagline)')
+    .addClass('not-selected hover');
+});
 
 $('.close').on('click', function() {
-  $(".not-selected").removeClass("not-selected")
-  $(this).parent().removeClass('selected').addClass('hover')
-})
+  $('.not-selected').removeClass('not-selected');
+  $(this)
+    .parent()
+    .removeClass('selected')
+    .addClass('hover');
+});
+
+$('.project-link').on('click', function(event) {
+  event.preventDefault();
+  $('.projects-menu-tag').click();
+});
+
+$('.contact-link').on('click', function(event) {
+  event.preventDefault();
+  $('.contact-menu-tag').click();
+});
